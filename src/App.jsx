96a176b2236d1f2939caea6b9a326cd4e350b61f
widipuriarto <widipuriarto@gmail.com>
2026-03-16 23:36:03 +0700

    ubah teks di app

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1 className='bg-blue-200'>Vite + React</h1>
+      <h3>Puriarto Bagas Widiantoro</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
