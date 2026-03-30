fe7a031de793c77563c1c2e7f0636c01d48446e3
widipuriarto <widipuriarto@gmail.com>
2026-03-31 01:08:41 +0700

    menggunakan utility class tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="min-h-screen bg-neutral-800 flex flex-col items-center justify-center text-center">
+        <div className="flex items-center justify-center space-x-8 mb-12">
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="w-24 h-24" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo}className="w-24 h-24 animate-spin" alt="React logo" style={{animationDuration: "10s"}} />
+          </a>
+        </div>
+        <h1 className="text-white text-6xl font-bold mb-16">Vite + React</h1>
+        <div className="text-sm space-y-6 mb-8">
+          <button onClick={() => setCount((count) => count + 1)} className="bg-neutral-500 text-white py-2 px-6 rounded-lg text-lg">count is {count}</button>
+          <p className="text-neutral-400">
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="text-neutral-400">Click on the Vite and React logos to learn more</p>
       </div>
-      <h1 className='bg-blue-200'>Vite + React</h1>
-      <h3>Puriarto Bagas Widiantoro</h3>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
