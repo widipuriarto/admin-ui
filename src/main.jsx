1cf30a546d22b1bbad918980e7c22f820c627987
Widi Puriarto <widipuriarto@gmail.com>
2026-06-09 09:11:11 +0700

    practice login using jwt endpoint

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,12 @@ import App from "./App.jsx";
 import Parent from "./latihan/Parent.jsx";
 import { CounterContextProvider } from "./context/counterContext.jsx";
 import { ThemeContextProvider } from "./context/themeContext.jsx";
+import Form from "./latihan/Form.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeContextProvider>
-      <App />
+      <Form />
     </ThemeContextProvider>
   </StrictMode>,
 );
